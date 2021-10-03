a3087968e4de78e249fd4882f0e9b6d0743ce2e6
Daniel Sheward <20735168+daniel-sheward@users.noreply.github.com>
2021-10-03 05:27:36 -0700

    Modify message to test build and deploy

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     unique_id = os.getenv("UNIQUE_ID")
-    return f"Hello Kube! from {unique_id}!\n"
+    return f"Hello Kubernetes! from {unique_id}!\n"
 
 
 app.run(host="0.0.0.0", port=80)
